61d27b932d5f86f61db2f5bf981b6b0978a45ff1
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-21 18:38:11 +0100

    feat(MINOR): remove auth check from single user file obtaining

--- src/routes/fileRoutes.ts
+++ src/routes/fileRoutes.ts
@@ -21,10 +21,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-router.use(authenticateToken);
-router.post("/upload", upload.single("file"), uploadFile);
-router.get("/files", listFiles);
+router.post("/upload", authenticateToken, upload.single("file"), uploadFile);
+router.get("/files", authenticateToken, listFiles);
 router.get("/files/:userId/:filename", getFile);
-router.delete("/delete/:filename", deleteFile);
+router.delete("/delete/:filename", authenticateToken, deleteFile);
 
 export default router;
