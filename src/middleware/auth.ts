c9cf4d93fb0d6f10c6d436bdd2acbf5896041688
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-21 08:33:07 +0100

    refactor: replace token obtaining from auth header to cookie

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,23 +1,31 @@
-import { Request, Response, NextFunction } from 'express'
-import jwt from 'jsonwebtoken'
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 interface JwtPayload {
-    userId: string
+    userId: string;
 }
 
-export const authenticateToken = (req: Request & { userId?: string }, res: Response, next: NextFunction): any => {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
+export const authenticateToken = (
+    req: Request & { userId?: string },
+    res: Response,
+    next: NextFunction
+) => {
+    const { sessionId: token } = req.cookies;
 
     if (!token) {
-        return res.status(401).json({ error: 'Access token required' })
+        return res
+            .status(401)
+            .json({ status: 401, message: "Authorization token not provided" });
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload
-        req.userId = decoded.userId
-        next()
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET!
+        ) as JwtPayload;
+        req.userId = decoded.userId;
+        next();
     } catch (error) {
-        return res.status(403).json({ error: 'Invalid token' })
+        return res.status(403).json({ error: "Invalid token" });
     }
-}
+};
