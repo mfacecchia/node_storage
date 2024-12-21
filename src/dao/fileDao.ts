c4d24f0cc82dc757c28e7ba91e27bff119b40597
Marco Facecchia <facecchiamarco@icloud.com>
2024-12-22 00:11:20 +0100

    feat(MINOR): add prisma queries errors handling

--- src/dao/fileDao.ts
+++ src/dao/fileDao.ts
@@ -1,10 +1,24 @@
 import { File, PrismaClient } from "@prisma/client";
+import {
+    PrismaClientKnownRequestError,
+    PrismaClientValidationError,
+} from "@prisma/client/runtime/library";
+import { DatabaseQueryError, NotFoundError } from "../errors/custom.errors";
 
 const prisma = new PrismaClient();
 
 export class FileDAO {
     async createFile(data: Omit<File, "id" | "shared_at">) {
-        return await prisma.file.create({ data });
+        try {
+            return await prisma.file.create({ data });
+        } catch (err) {
+            if (err instanceof PrismaClientValidationError) {
+                throw new DatabaseQueryError(
+                    "Invalid or missing required data"
+                );
+            }
+            throw err;
+        }
     }
 
     async findFiles(userId: File["user_id"], classId?: File["class_id"]) {
@@ -29,10 +43,19 @@ export class FileDAO {
     }
 
     async deleteFileById(fileId: File["id"]) {
-        return await prisma.file.delete({
-            where: {
-                id: fileId,
-            },
-        });
+        try {
+            return await prisma.file.delete({
+                where: {
+                    id: fileId,
+                },
+            });
+        } catch (err) {
+            if (err instanceof PrismaClientKnownRequestError) {
+                if (err.code == "P2025") {
+                    throw new NotFoundError("File does not exist");
+                }
+            }
+            throw err;
+        }
     }
 }
